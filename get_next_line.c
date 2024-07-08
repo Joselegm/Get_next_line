9865a844e6d7a73ae898c9026d4e29f6741edddc
joselegm <joselegm@iMac-de-Jose.local>
2024-07-08 19:28:20 +0200

    gnl2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: joselegm <joselegm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:56:01 by joselegm          #+#    #+#             */
-/*   Updated: 2024/07/04 19:19:01 by joselegm         ###   ########.fr       */
+/*   Updated: 2024/07/08 19:26:15 by joselegm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,29 @@ char *ft_readline(char *str)
     new_line = (char *)malloc(sizeof(char) * i+1);
     if (!new_line)
         return(NULL);
+    i = 0;
+    while (str[i] && str[i] != '\n')
+    {
+        new_line[i] = str[i];
+        i++;
+    }
+    if (str[i] == '\n')
+        new_line[i++] = '\n';
+    new_line[i] = '\0';
+    return (new_line);
+}
+char *ft_new_line(char *str1)
+{
+    int i;
+    int k;
+    char * new_file;
     
-    
+    i = 0;
+    while (str1[i] && str1[i] != '\n')
+        i++;
+    if (!str1[i])
+    {
+        free(str1);
+        return(NULL);
+    }
 }
