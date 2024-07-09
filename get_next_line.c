e597b5c506886fe2c18478566e11920026e21338
joselegm <joselegm@iMac-de-Jose.local>
2024-07-09 19:02:48 +0200

    gnl3

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: joselegm <joselegm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:56:01 by joselegm          #+#    #+#             */
-/*   Updated: 2024/07/08 19:26:15 by joselegm         ###   ########.fr       */
+/*   Updated: 2024/07/09 19:00:40 by joselegm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "get_next_line.h"
+
 char *ft_readline(char *str)
 {
     int i;
@@ -23,7 +24,7 @@ char *ft_readline(char *str)
         i++;
     if (str[i] == "\n")
         i++;
-    new_line = (char *)malloc(sizeof(char) * i+1);
+    new_line = (char *)malloc(sizeof(char) * i + 1);
     if (!new_line)
         return(NULL);
     i = 0;
@@ -49,6 +50,19 @@ char *ft_new_line(char *str1)
     if (!str1[i])
     {
         free(str1);
-        return(NULL);
+        return (NULL);
     }
+    i += (str1[i] == '\n');
+    new_file = (char *)malloc(sizeof(char) * (ft_strlen(str1) -i + 1));
+    if (!new_file)
+        return (NULL);
+    k = 0;
+    while (str1[i + k])
+    {
+        new_file[k] = str1[i + k];
+        k++;
+    }
+    new_file[k] = '\0';
+    //free (str1);  (???)
+    return (new_file);
 }
